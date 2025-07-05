6c64ce9a8bbcf86bec0cacc9b2ad8b6481e6570a
NeoThePotato <neorayb@gmail.com>
2025-07-05 16:17:53 +0300

    Working UnitTests.

--- Assets/Tests/UnitTests.cs
+++ Assets/Tests/UnitTests.cs
@@ -5,22 +5,41 @@ using NUnit.Framework;
 
 public class UnitTests
 {
-    private Torch _torch;
+	private static readonly Shader TORCH_SHADER = Shader.Find("Universal Render Pipeline/Lit");
+	private static readonly System.Type[] REQUIRED_COMPONENTS = { typeof(Torch), typeof(MeshRenderer) };
 
-    [UnitySetUp]
-    public IEnumerator SetUp()
-    {
+	private Torch _torch;
+
+	[UnitySetUp]
+	public IEnumerator SetUp()
+	{
 		yield return null;
-		_torch = Object.FindFirstObjectByType<Torch>();
-        Assert.IsNotNull(_torch, "No active torch found in scene.");
+		var go = new GameObject("Test Torch", REQUIRED_COMPONENTS);
+		_torch = go.GetComponent<Torch>();
+		_torch.rend = go.GetComponent<MeshRenderer>();
+		_torch.rend.material = new(TORCH_SHADER);
+	}
+
+	[UnityTearDown]
+	public IEnumerator TearDown()
+	{
+		yield return null;
+		Object.Destroy(_torch.gameObject);
 	}
 
 	[UnityTest]
-    public IEnumerator TorchStaysLitAfterCallingLightUp()
+	public IEnumerator TorchIsUnlitByDefault()
+	{
+		yield return null;
+		Assert.IsFalse(_torch.isLit, "Torch.isLit is true without Torch.LightUp.");
+	}
+
+	[UnityTest]
+	public IEnumerator TorchStaysLitAfterCallingLightUp()
 	{
 		yield return null;
 		_torch.LightUp();
-        Assert.IsTrue(_torch.isLit, "Torch.isLit is not true after calling Torch.LightUp.");
+		Assert.IsTrue(_torch.isLit, "Torch.isLit is false after calling Torch.LightUp.");
 	}
 
 	[UnityTest]
@@ -33,11 +52,17 @@ public class UnitTests
 	}
 
 	[UnityTest]
+	public IEnumerator UnLitTorchRendersCorrectly()
+	{
+		yield return null;
+		Assert.AreEqual(_torch.rend.material.color, Torch.UNLIT_COLOR, "Unlit Torch renderer has incorrect color.");
+	}
+
+	[UnityTest]
 	public IEnumerator LitTorchRendersCorrectly()
 	{
 		yield return null;
-		Assert.IsNotNull(_torch.rend, "Torch has no renderer.");
-		Assert.IsNotNull(_torch.rend.material, "Torch.rend has no material.");
-		Assert.AreEqual(_torch.rend.material.color, Torch.LIT_COLOR, "Torch renderer has incorrect color.");
+		_torch.LightUp();
+		Assert.AreEqual(_torch.rend.material.color, Torch.LIT_COLOR, "Lit Torch renderer has incorrect color.");
 	}
 }
