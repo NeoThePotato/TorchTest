babc2f8d833b99f2e3e8bc8cf11b76a642a8246e
NeoThePotato <neorayb@gmail.com>
2025-07-19 23:58:52 +0300

    Re-order operands in MoveBind.

--- Assets/Scripts/Torch/MoveBind.cs
+++ Assets/Scripts/Torch/MoveBind.cs
@@ -7,13 +7,7 @@ public class MoveBind : MonoBehaviour
 	public float speed = 5f;
 	private Vector2 input;
 
-	public void OnMove(InputAction.CallbackContext context)
-	{
-		input = context.ReadValue<Vector2>();
-	}
+	public void OnMove(InputAction.CallbackContext context) => input = context.ReadValue<Vector2>();
 
-	public void FixedUpdate()
-	{
-		characterController.Move(new Vector3(input.x, 0f, input.y) * Time.deltaTime * speed);
-	}
+	public void FixedUpdate() => characterController.Move(speed * Time.deltaTime * new Vector3(input.x, 0f, input.y));
 }
