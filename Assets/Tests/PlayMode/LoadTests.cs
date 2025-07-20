60bf3374bade102773a2841880575fd65f6cdace
NeoThePotato <neorayb@gmail.com>
2025-07-20 03:36:47 +0300

    Break off the higher torch spawn count into StressTests.

--- Assets/Tests/PlayMode/LoadTests.cs
+++ Assets/Tests/PlayMode/LoadTests.cs
@@ -5,36 +5,29 @@ using UnityEngine.TestTools;
 
 public class LoadTests
 {
-	private const int MAX_TORCH_COUNT = 100_000;
+	private const int MAX_TORCH_COUNT = 100;
 
 	private readonly List<Torch> _torches = new(MAX_TORCH_COUNT);
 
-	private IEnumerator SpawnAndLitTorch(int iterations)
+	public static IEnumerator SpawnAndLitTorch(IList<Torch> torches, int iterations)
 	{
 		for (int i = 0; i < iterations; i++)
-			_torches.Add(UnitTests.CreateTorch());
+			torches.Add(UnitTests.CreateTorch());
 		yield return null;
-		foreach (var t in _torches)
+		foreach (var t in torches)
 			t.LightUp();
 		yield return null;
-		foreach (var t in _torches)
+		foreach (var t in torches)
 			Object.Destroy(t.gameObject);
-		_torches.Clear();
+		torches.Clear();
 	}
+
+	private IEnumerator SpawnAndLitTorch(int iterations) => SpawnAndLitTorch(_torches, iterations);
 
 	[UnityTest]
 	public IEnumerator SpawnAndLitTorch10() => SpawnAndLitTorch(10);
 
 	[UnityTest]
-	public IEnumerator SpawnAndLitTorch100() => SpawnAndLitTorch(100);
-
-	[UnityTest]
-	public IEnumerator SpawnAndLitTorch1K() => SpawnAndLitTorch(1_000);
-
-	[UnityTest]
-	public IEnumerator SpawnAndLitTorch10K() => SpawnAndLitTorch(10_000);
-
-	[UnityTest]
-	public IEnumerator SpawnAndLitTorch100K() => SpawnAndLitTorch(100_000);
+	public IEnumerator SpawnAndLitTorch100() => SpawnAndLitTorch(MAX_TORCH_COUNT);
 
 }
